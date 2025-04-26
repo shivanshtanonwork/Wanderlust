82e882d4dd610752e82090690e603e9a57c744c7
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-04-26 08:13:31 +0530

    Basic Setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+
+main()
+    .then(() => {
+        console.log("connected to DB")
+    }).catch((err) => {
+        console.log(err)
+    })
+
+async function main() {
+    await mongoose.connect(MONGO_URL)
+}
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am Root")
+})
+
+app.listen(8080, () => {
+    console.log("App is listening on port 8080");
+})
