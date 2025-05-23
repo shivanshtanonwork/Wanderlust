9d51690ad4601167008c7706da063dd92468ab3a
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-23 12:30:20 +0530

    Validation for Schema Middleware

--- app.js
+++ app.js
@@ -31,6 +31,16 @@ app.get("/", (req, res) => {
     res.send("Hi, I am root")
 })
 
+const validateListing = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body)
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, errMsg)
+    } else {
+        next()
+    }
+}
+
 //Index Route
 app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({})
@@ -50,12 +60,7 @@ app.get("/listings/:id", wrapAsync(async (req, res) => {
 }))
 
 //Create Route
-app.post("/listings", wrapAsync(async (req, res, next) => {
-    let result = listingSchema.validate(req.body)
-    console.log(result)
-    if (result.error) {
-        throw new ExpressError(400, result.error)
-    }
+app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
     // let { title, description, image, price, country, location } = req.body
     const newListing = new Listing(req.body.listing)
     await newListing.save()
@@ -70,7 +75,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }))
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateListing, wrapAsync(async (req, res) => {
     if (!req.body.listing) {
         throw new ExpressError(400, "Send valid data for listing")
     }
