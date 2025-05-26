92d880d3e5f410793a23166d8bc7b4a0a4994a3a
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-26 19:18:05 +0530

    Create Reviews and stored it to DB

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const ejsMate = require("ejs-mate")
 const wrapAsync = require("./utils/wrapAsync")
 const ExpressError = require("./utils/ExpressError")
 const { listingSchema } = require("./schema")
+const Review = require("./models/review")
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
 async function main() {
@@ -91,6 +92,21 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
     console.log(deletedListing)
     res.redirect("/listings")
 }))
+
+//Reviews
+//Post Route
+app.post("/listings/:id/reviews", async (req, res) => {
+    let listing = await Listing.findById(req.params.id)
+    let newReview = new Review(req.body.review)
+
+    listing.reviews.push(newReview)
+
+    await newReview.save()
+    await listing.save()
+    // console.log("new review saved")
+    // res.send("New Review Saved")
+    res.redirect(`/listings/${listing._id}`)
+})
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
