2b26b188058ecefffde65d0cf214ec0378b4aefb
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-26 22:24:02 +0530

    Validation for Reviews

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const wrapAsync = require("./utils/wrapAsync")
 const ExpressError = require("./utils/ExpressError")
-const { listingSchema } = require("./schema")
+const { listingSchema, reviewSchema } = require("./schema")
 const Review = require("./models/review")
 
 const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust'
@@ -34,6 +34,16 @@ app.get("/", (req, res) => {
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body)
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",")
+        throw new ExpressError(400, errMsg)
+    } else {
+        next()
+    }
+}
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body)
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",")
         throw new ExpressError(400, errMsg)
@@ -95,18 +105,20 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews", async (req, res) => {
-    let listing = await Listing.findById(req.params.id)
-    let newReview = new Review(req.body.review)
+app.post("/listings/:id/reviews",
+    validateReview,
+    wrapAsync(async (req, res) => {
+        let listing = await Listing.findById(req.params.id)
+        let newReview = new Review(req.body.review)
 
-    listing.reviews.push(newReview)
+        listing.reviews.push(newReview)
 
-    await newReview.save()
-    await listing.save()
-    // console.log("new review saved")
-    // res.send("New Review Saved")
-    res.redirect(`/listings/${listing._id}`)
-})
+        await newReview.save()
+        await listing.save()
+        // console.log("new review saved")
+        // res.send("New Review Saved")
+        res.redirect(`/listings/${listing._id}`)
+    }))
 
 // app.get("/testListing", async (req, res) => {
 //     let sampleListing = new Listing({
