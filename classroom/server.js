94b87df57fb9a247c4b4e697d0dedc9e0a2e1077
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-27 18:39:38 +0530

    Sending Cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const users = require("./routes/user")
 const posts = require("./routes/post")
+
+app.get("/getcookies", (req, res) => {
+    res.cookie("greet", "Hello")
+    res.send("Sent you some cookies!")
+})
+
 app.get("/", (req, res) => {
     res.send("Hi I'm Root")
 })
