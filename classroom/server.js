0ee2442070cf5aa69f1b949b59251deb9a7a0920
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-28 17:37:06 +0530

    Using connect-flash

--- classroom/server.js
+++ classroom/server.js
@@ -3,6 +3,50 @@ const app = express()
 const users = require("./routes/user")
 const posts = require("./routes/post")
 const session = require("express-session")
+const flash = require("connect-flash")
+const path = require("path")
+
+app.use("/posts", posts)
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const sessionOptions = {
+    secret: "mysupersecretstring",
+    resave: false,
+    saveUninitialized: true
+}
+app.use(session(sessionOptions))
+app.use(flash());
+
+app.get("/register", (req, res) => {
+    let { name = "anonymous" } = req.query;
+    req.session.name = name
+    req.flash('success', 'user registered successfully!')
+    res.redirect("/hello")
+})
+
+app.get("/hello", (req, res) => {
+    res.render("page.ejs", { name: req.session.name, msg: req.flash("success") })
+})
+
+// app.get("/reqcount", (req, res) => {
+//     if (req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`)
+// })
+
+// app.get("/test", (req, res) => {
+//     res.send("test successful!")
+// })
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+})
+
+
 // const cookieParser = require("cookie-parser")
 
 // app.use(cookieParser("secretcode"))
@@ -35,37 +79,4 @@ const session = require("express-session")
 // })
 
 // app.use("/users", users)
-// app.use("/posts", posts)
-
-const sessionOptions = {
-    secret: "mysupersecretstring",
-    resave: false,
-    saveUninitialized: true
-}
-app.use(session(sessionOptions))
-
-app.get("/register", (req, res) => {
-    let { name = "anonymous" } = req.query;
-    req.session.name = name
-    res.redirect("/hello")
-})
-
-app.get("/hello", (req, res) => {
-    res.send(`hello, ${req.session.name}`)
-})
-
-// app.get("/reqcount", (req, res) => {
-//     if (req.session.count) {
-//         req.session.count++;
-//     } else {
-//         req.session.count = 1;
-//     }
-//     res.send(`You sent a request ${req.session.count} times`)
-// })
-
-// app.get("/test", (req, res) => {
-//     res.send("test successful!")
-// })
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
-})
+//
