03a3704b58a7f8f36ff4b38400bbf5baeb37cb12
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-28 19:20:22 +0530

    Implementing Session in Wanderlust

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require("path")
 const methodOverride = require("method-override")
 const ejsMate = require("ejs-mate")
 const ExpressError = require("./utils/ExpressError")
+const session = require('express-session')
 
 
 const listings = require("./routes/listing")
@@ -29,6 +30,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+}
+
+app.use(session(sessionOptions))
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root")
