8c7ce4376fc49a2e57fdf02ce04af4dd0f9a50fc
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-05-30 12:55:35 +0530

    Connecting to Login Route

--- routes/listing.js
+++ routes/listing.js
@@ -4,6 +4,7 @@ const wrapAsync = require("../utils/wrapAsync")
 const ExpressError = require("../utils/ExpressError")
 const { listingSchema } = require("../schema")
 const Listing = require("../models/listing")
+const { isLoggedIn } = require("../middleware")
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body)
@@ -22,7 +23,7 @@ router.get("/", wrapAsync(async (req, res) => {
 }))
 
 //New/Create Route
-router.get("/new", (req, res) => {
+router.get("/new", isLoggedIn, (req, res) => {
     res.render("listings/new.ejs")
 })
 
@@ -39,7 +40,7 @@ router.get("/:id", wrapAsync(async (req, res) => {
 
 
 //Create Route
-router.post("/", validateListing, wrapAsync(async (req, res, next) => {
+router.post("/", isLoggedIn, validateListing, wrapAsync(async (req, res, next) => {
     // let { title, description, image, price, country, location } = req.body
     const newListing = new Listing(req.body.listing)
     await newListing.save()
@@ -48,7 +49,7 @@ router.post("/", validateListing, wrapAsync(async (req, res, next) => {
 }))
 
 //Edit Route 
-router.get("/:id/edit", wrapAsync(async (req, res) => {
+router.get("/:id/edit", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params
     const listing = await Listing.findById(id)
     if (!listing) {
@@ -59,7 +60,7 @@ router.get("/:id/edit", wrapAsync(async (req, res) => {
 }))
 
 //Update Route
-router.put("/:id", validateListing, wrapAsync(async (req, res) => {
+router.put("/:id", isLoggedIn, validateListing, wrapAsync(async (req, res) => {
     if (!req.body.listing) {
         throw new ExpressError(400, "Send valid data for listing")
     }
@@ -70,7 +71,7 @@ router.put("/:id", validateListing, wrapAsync(async (req, res) => {
 }))
 
 //Delete Route
-router.delete("/:id", wrapAsync(async (req, res) => {
+router.delete("/:id", isLoggedIn, wrapAsync(async (req, res) => {
     let { id } = req.params
     let deletedListing = await Listing.findByIdAndDelete(id)
     console.log(deletedListing)
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const User = require("../models/user");
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs")
@@ -19,7 +20,20 @@ router.post("/signup", wrapAsync(async (req, res) => {
         req.flash("error", e.message)
         res.redirect("/signup")
     }
-
 }))
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs")
+})
+
+router.post("/login",
+    passport.authenticate("local", {
+        failureRedirect: '/login',
+        failureFlash: true
+    }),
+    async (req, res) => {
+        req.flash("success", "Welcome back to Wanderlust!")
+        res.redirect("/listings")
+    })
 
 module.exports = router;
