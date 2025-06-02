9e1c5f298f28debfce67f03c6a703c87301140e0
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-03 02:28:49 +0530

    Map Marker

--- public/js/map.js
+++ public/js/map.js
@@ -2,6 +2,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: "map", // container ID
-    center: [79.9300962, 23.1599217], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
     zoom: 9, // starting zoom
 });
+
+console.log(coordinates)
+
+const marker = new mapboxgl.Marker({ color: "red" })
+    .setLngLat(coordinates)  //Listing.geometry.coordinates
+    .addTo(map);
