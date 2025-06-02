980fdb8125008d537289dd420434e5876f17f07f
Shivansh Tandon <shivanshtandonwork@gmail.com>
2025-06-02 18:30:32 +0530

    Edit Listing Image

--- controllers/listings.js
+++ controllers/listings.js
@@ -55,7 +55,15 @@ module.exports.updateListing = async (req, res) => {
         throw new ExpressError(400, "Send valid data for listing")
     }
     let { id } = req.params
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing })
+    let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing })
+
+    if (typeof req.file !== "undefined") {
+        let url = req.file.path
+        let filename = req.file.filename
+        listing.image = { url, filename }
+        await listing.save()
+    }
+
     req.flash("success", "Listing Updated!")
     res.redirect(`/listings/${id}`)
 }
